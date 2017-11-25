4343b9295ac01ad1576d1ef0ce52f57b3a37915c
Jcct100 <jamestang_@outlook.com>
2017-11-25 14:12:38 +0000

    made changes to index.html

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ angular
   .module('front', [])
   .controller('MainCtrl', MainCtrl);
 
+MainCtrl.$inject = ['$http'];
 function MainCtrl($http) {
   const vm = this;
 
@@ -9,5 +10,14 @@ function MainCtrl($http) {
     .get('http://localhost:3000/api/restaurants')
     .then(res => {
       vm.restaurants = res.data;
+      console.log(vm.restaurants);
     });
+
+$http
+  .get('http://localhost:3000/api/reviews')
+  .then(res => {
+    vm.reviews = res.data;
+    console.log(vm.reviews);
+  });
+
 }
